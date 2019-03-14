cd670d01c5575ee5d1bce2185f32bca79eea51e3
nathanlieu21 <yime@vcu.edu>
2019-03-14 19:11:57 -0400

    Testing update login and registration

--- app/src/main/java/com/example/workclout/LoginActivity.java
+++ app/src/main/java/com/example/workclout/LoginActivity.java
@@ -1,21 +1,67 @@
 package com.example.workclout;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.Toast;
 
 
+
+
+
+
 public class LoginActivity extends AppCompatActivity{
+    private EditText userName, passWord;
+    private Button register, login;
+    private String userNameInput, passWordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        userName=(EditText)findViewById(R.id.UserNameID);
+        passWord=(EditText)findViewById(R.id.PassWordID);
+        register=(Button)findViewById(R.id.RegisterID);
+        login=(Button)findViewById(R.id.LogID);
 
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+       login.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+
+                //userNameInput=userName.getText().toString();// takes input
+                //passWordInput=passWord.getText().toString();// takes input
+
+               // add firebase rehgistration 
+               if((userName.getText().toString().equals("John")&& passWordInput.equals("Fuck")))
+               {
+                   Intent homepage = new Intent(LoginActivity.this, HomePage.class);
+                   startActivity(homepage);
+                   Toast.makeText(LoginActivity.this, "You're login works", Toast.LENGTH_SHORT).show();
+
+
+               }
+               else
+               {
+                   Toast.makeText(LoginActivity.this, "You're login infromation does not match", Toast.LENGTH_SHORT).show();
+               }
+           }
+       });
     }
+
+
+
 }
 
---
+++ app/src/main/java/com/example/workclout/Registration.java
@@ -0,0 +1,58 @@
+package com.example.workclout;
+
+import android.media.MediaPlayer;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class Registration extends AppCompatActivity {
+   // private FirebaseAuthException firebase;
+    private EditText userName, passWord, RePassWord;
+    private String userNameInput, passWordInput, rePassWordInput;
+    private Button register;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        userName=(EditText)findViewById(R.id.UserNameID);
+        passWord=(EditText)findViewById(R.id.PassWordID);
+        passWord=(EditText)findViewById(R.id.PassWordID);
+        register=(Button)findViewById(R.id.RegisterID);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userNameInput=userName.getText().toString();// takes input
+                passWordInput=passWord.getText().toString();// takes input
+
+                firebaseAuth.createUserWithEmailAndPassword(userNameInput,passWordInput).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Toast.makeText(Registration.this, "You're login works", Toast.LENGTH_SHORT).show();
+
+
+                    }
+                };
+
+            }
+        });
+
+    }
+}
