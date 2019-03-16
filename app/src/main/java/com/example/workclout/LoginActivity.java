6ff9db14166eafb0884f06d69d97750c963fb3cb
napierbw <napierbw@mymail.vcu.edu>
2019-03-16 18:42:33 -0400

    Added dependencies for testing. Added documentation. Added updated UI for registration activity.

--- app/src/main/java/com/example/workclout/LoginActivity.java
+++ app/src/main/java/com/example/workclout/LoginActivity.java
@@ -19,11 +19,6 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-
-
-
-
-
 public class LoginActivity extends AppCompatActivity{
     private EditText userName, passWord;
     private Button register, login;
