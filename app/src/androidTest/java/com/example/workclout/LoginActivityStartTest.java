eeb9a88f59d2f20f9a1ee65d53944169ca069c66
napierbw <napierbw@mymail.vcu.edu>
2019-03-16 21:01:53 -0400

    Added tests for startup of activities and more tests for other error cases.

---
+++ app/src/androidTest/java/com/example/workclout/LoginActivityStartTest.java
@@ -0,0 +1,48 @@
+package com.example.workclout;
+
+/*
+ * Tests that Login Activity starts properly
+ * @author Benjamin Napier
+ */
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class LoginActivityStartTest {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityTestRule = new ActivityTestRule<LoginActivity>(LoginActivity.class);
+
+    private LoginActivity mActivity = null;
+
+    @Before
+    public void setUp() throws Exception {
+        mActivity = mActivityTestRule.getActivity();
+    }
+
+    @Test
+    public void testLaunch(){
+        View view = mActivity.findViewById(R.id.et_username);
+
+        assertNotNull(view);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mActivity = null;
+    }
+}
