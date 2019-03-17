7dcc842da84a5472a8ef87c5693ba58a0b7d87b2
Brian Zhang <zhangb8@vcu.edu>
2019-03-16 22:09:04 -0400

    Added Coach and Athlete Registration

--- app/src/main/java/com/example/workclout/HomePage.java
+++ app/src/main/java/com/example/workclout/HomePage.java
@@ -13,8 +13,6 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
     }
 
