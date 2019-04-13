8d65096c0fc4a83e718f084232f7f582e6e8fc59
Brian Zhang <zhangb8@vcu.edu>
2019-04-13 15:12:39 -0400

    Verify Email

--- app/src/main/java/com/example/workclout/Verify.java
+++ app/src/main/java/com/example/workclout/Verify.java
@@ -6,11 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class Verify extends AppCompatActivity {
 
     private EditText code;
     private Button verify, resend;
+    public helperClass x = new helperClass();
+    private String verifyCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,17 +29,40 @@ public class Verify extends AppCompatActivity {
         resend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO: logic for resending verify code here
+                String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+                Random random = new Random();
+                StringBuilder builder = new StringBuilder(6);
+                for (int i = 0; i < 6; i++) {
+                    builder.append(ALPHABET.charAt(random.nextInt(ALPHABET.length())));
+                }
+
+                verifyCode = builder.toString();
+                Toast.makeText(Verify.this, verifyCode, Toast.LENGTH_SHORT).show();
+
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("message/rfc822");
+                i.putExtra(Intent.EXTRA_EMAIL  , new String[]{x.getEmail()});
+                i.putExtra(Intent.EXTRA_SUBJECT, "Welcome to WorkClout");
+                i.putExtra(Intent.EXTRA_TEXT   , "Your verification code is:" + "\n" + "\n" + verifyCode);
+                try {
+                    startActivity(Intent.createChooser(i, "Send mail..."));
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(Verify.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
         verify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO: logic for checking verify code here
-
-                Intent loginSuccess = new Intent(Verify.this, SetupProfile.class);
-                startActivity(loginSuccess);
+                if (code.getText().toString().equals(verifyCode)) {
+                    Intent loginSuccess = new Intent(Verify.this, SetupProfile.class);
+                    startActivity(loginSuccess);
+                }
+                else {
+                    Toast.makeText(Verify.this, "Entered code is incorrect", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
